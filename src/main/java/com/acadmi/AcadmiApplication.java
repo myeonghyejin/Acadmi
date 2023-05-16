0780efd3e87016a8ed7aaed7546b58c8e545fc04
YoungDHi <sm930219@naver.com>
2023-05-17 00:57:39 +0900

    Start

---
+++ src/main/java/com/acadmi/AcadmiApplication.java
@@ -0,0 +1,13 @@
+package com.acadmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcadmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcadmiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acadmi/AcadmiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acadmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcadmiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
