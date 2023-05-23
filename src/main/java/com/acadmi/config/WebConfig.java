071108327f7464979bdb8139f9930c3a23352736
yangjinh94259 <yangjinh94259@gmail.com>
2023-05-23 11:03:54 +0900

    member

--- src/main/java/com/acadmi/config/WebConfig.java
+++ src/main/java/com/acadmi/config/WebConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 //**-context.xml
 public class WebConfig implements WebMvcConfigurer {
 	
--- src/main/java/com/acadmi/member/MemberService.java
+++ src/main/java/com/acadmi/member/MemberService.java
@@ -1,6 +1,7 @@
 package com.acadmi.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 public class MemberService {
 	
