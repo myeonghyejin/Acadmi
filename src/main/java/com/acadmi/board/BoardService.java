589346de5bae6e7d3fffc9494c78b5e0e3ad2b5b
JangHs2021 <wkdgustj02@naver.com>
2023-05-24 09:16:39 +0900

    Qna

--- src/main/java/com/acadmi/board/BoardService.java
+++ src/main/java/com/acadmi/board/BoardService.java
@@ -10,18 +10,13 @@ public interface BoardService {
 	
 	public List<BoardVO> getList(Pagination pagination) throws Exception;
 		
-	// 글 하나 조회
 	public BoardVO getDetail(BoardVO boardVO) throws Exception;
 			
-	// 글쓰기
 	public int setInsert(BoardVO boardVO, MultipartFile [] multipartFiles) throws Exception;
-			
-	// 글수정
+
 	public int setUpdate(BoardVO boardVO) throws Exception;
 			
-	// 글삭제
 	public int setDelete(BoardVO boardVO) throws Exception;
 		
-	// 파일조회
 	public BoardFileVO getFileDetail(BoardFileVO boardFileVO) throws Exception;
 }
