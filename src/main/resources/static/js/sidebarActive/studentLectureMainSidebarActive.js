c4b974def997758f20d6d0eba8c2d97bf6927d16
YoungDHi <sm930219@naver.com>
2023-06-20 01:47:45 +0900

    secondSidebarActive

--- src/main/resources/static/js/sidebarActive/studentLectureMainSidebarActive.js
+++ src/main/resources/static/js/sidebarActive/studentLectureMainSidebarActive.js
@@ -1,7 +1,13 @@
 /**
- * professor 사이드바 active 표시
+ * student 사이드바 active 표시
  */
 
 if(window.location.pathname == '/student/lecture/syllabusDetail'){
 	$('#studentSyllabusDetail').addClass('active')
+} else if (window.location.pathname == '/student/lecture/attendee'){
+	$('#studentAttendeeList').addClass('active')
+} else if(window.location.pathname.startsWith('/lectureNotice')){
+	$('#lectureNoticeList').addClass('active')
+} else if(window.location.pathname.startsWith('/lectureQna')){
+	$('#lectureQnaList').addClass('active')
 }
